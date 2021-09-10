cf2ada95cf499ab873d333916abb18c86c8425e8
leejiwonn <bbongwa@naver.com>
2021-09-10 17:32:11 +0900

    fix: `reset css` , `history` , 로그인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+
+const reset = css`
+  ${emotionReset}
+  html,
+  body {
+    height: 100%;
+  }
+  #root {
+    width: 100%;
+    height: 100%;
+    font-family: 'Noto Sans', sans-serif;
+    color: #252831;
+  }
+  button {
+    border: none;
+    padding: 0;
+    font-family: 'Noto Sans', sans-serif;
+    color: #252831;
+  }
+  a {
+    text-decoration: none;
+    color: #252831;
+  }
+`;
+
+const history = createBrowserHistory({ basename: '/' });
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact={true} path="/" component={Home} />
-        {/* Not Found */}
-        <Route component={() => <Redirect to="/" />} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Global styles={reset} />
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+          {/* Not Found */}
+          <Route component={() => <Redirect to="/" />} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <LoginStyled>
+      <Link to="/home">
+        <LoginButton>로그인</LoginButton>
+      </Link>
+    </LoginStyled>
+  );
+};
+
+const LoginStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginButton = styled.button`
+  font-size: 24px;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
+export default Login;
