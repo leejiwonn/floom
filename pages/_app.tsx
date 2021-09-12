e1af11eb5a5155ae25cb5bf7acdeb02ce6c6a6be
leejiwonn <bbongwa@naver.com>
2021-09-13 02:30:33 +0900

    fix: `emotion-reset` 제거 후 `global.css` 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,6 @@
-import emotionReset from 'emotion-reset';
-import { Global, css } from '@emotion/react';
 import { AppProps } from 'next/app';
-
-const reset = css`
-  ${emotionReset}
-  html,
-  body {
-    height: 100%;
-  }
-  #root {
-    width: 100%;
-    height: 100%;
-    font-family: 'Noto Sans', sans-serif;
-    color: #252831;
-  }
-  button {
-    border: none;
-    padding: 0;
-    font-family: 'Noto Sans', sans-serif;
-    color: #252831;
-  }
-  a {
-    text-decoration: none;
-    color: #252831;
-  }
-`;
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Global styles={reset} />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
