54879ca652cb939380cf2f63bb03e4a40defbc56
leejiwonn <bbongwa123@gmail.com>
2021-10-08 00:41:31 +0900

    fix: `Dropdown` 컴포넌트 개선 - `babel-plugin-inline-react-svg` 패키지 추가

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,35 +1,107 @@
 import styled from '@emotion/styled';
+import { useState, useEffect, useRef } from 'react';
+
+import Typography from './Typography';
+
+import { FontType } from '~/utils/font';
+import { TextColor } from '~/utils/color';
+import DropdownIcon from '../../public/assets/icons/icon-dropdown.svg';
 
 interface Props {
+  time: number;
   onChangeTime: (value: number) => void;
 }
 
-// TODO : 커스텀 형태로 변경 필요
-const Dropdown = ({ onChangeTime }: Props) => {
-  const handleTimeValueClick = (e: any) => {
-    onChangeTime(e.target.value);
+const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+
+const Dropdown = ({ time, onChangeTime }: Props) => {
+  const [isActive, setIsActive] = useState(false);
+  const modalRef = useRef(null);
+
+  const handleTimeValueClick = (value: number) => {
+    onChangeTime(value);
+    setIsActive(false);
   };
 
+  const handleClickOutside = (e: any) => {
+    if (!modalRef.current?.contains(e.target)) {
+      setIsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <DropdownStyled name="time" onChange={(e) => handleTimeValueClick(e)}>
-      <DropdownItem value={10}>10</DropdownItem>
-      <DropdownItem value={20}>20</DropdownItem>
-      <DropdownItem value={30}>30</DropdownItem>
-      <DropdownItem value={40}>40</DropdownItem>
-      <DropdownItem value={50}>50</DropdownItem>
-      <DropdownItem value={60}>60</DropdownItem>
-      <DropdownItem value={70}>70</DropdownItem>
-      <DropdownItem value={80}>80</DropdownItem>
-      <DropdownItem value={90}>90</DropdownItem>
-      <DropdownItem value={100}>100</DropdownItem>
-      <DropdownItem value={110}>110</DropdownItem>
-      <DropdownItem value={120}>120</DropdownItem>
+    <DropdownStyled ref={modalRef}>
+      <DropdownButton onClick={() => setIsActive((prev) => !prev)}>
+        <Typography
+          font={FontType.EXTRA_BOLD_HEAD_03}
+          color={TextColor.SECONDARY}
+        >
+          {time}분
+        </Typography>
+        <DropdownIconStyled active={isActive}>
+          <DropdownIcon />
+        </DropdownIconStyled>
+      </DropdownButton>
+      <DropdownBox active={isActive}>
+        {values.map((value, index) => (
+          <DropdownItem
+            key={index}
+            onClick={() => handleTimeValueClick(value)}
+            selected={time === value}
+          >
+            {value}분
+          </DropdownItem>
+        ))}
+      </DropdownBox>
     </DropdownStyled>
   );
 };
 
-const DropdownStyled = styled.select``;
+const DropdownStyled = styled.div`
+  width: 120px;
+  height: 50px;
+  display: inline-block;
+  border-radius: 10px;
+  background-color: #f6f6f6;
+`;
 
-const DropdownItem = styled.option``;
+const DropdownButton = styled.button`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+`;
+
+const DropdownIconStyled = styled.span<{ active: boolean }>`
+  transform: ${({ active }) => (active ? 'rotate(180deg)' : 'rotate(0deg)')};
+`;
+
+const DropdownBox = styled.div<{ active: boolean }>`
+  width: 100%;
+  height: 100px;
+  display: ${({ active }) => (active ? 'flex' : 'none')};
+  flex-direction: column;
+  overflow: scroll;
+  background-color: #fff;
+  border: 2px solid #f6f6f6;
+  border-radius: 10px;
+  padding: 0 10px;
+  margin-top: 5px;
+`;
+
+const DropdownItem = styled.button<{ selected: boolean }>`
+  color: ${({ selected }) =>
+    selected ? TextColor.PRIMARY : TextColor.SECONDARY};
+`;
 
 export default Dropdown;
