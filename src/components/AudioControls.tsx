d568b6df327fa06244fd872c9d8d845b7f8d72d3
-g <-g>
2021-10-08 02:47:55 +0900

    fix: `AudioPlayer` 컴포넌트 (기능) 구현

---
+++ src/components/AudioControls.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+
+import Typography from './Typography';
+
+interface Props {
+  isPlaying: boolean;
+  onPlayPauseClick: () => void;
+  volume: number;
+  onAudioSoundControl: (value: number) => void;
+}
+
+const AudioControls = ({
+  isPlaying,
+  onPlayPauseClick,
+  volume,
+  onAudioSoundControl,
+}: Props) => {
+  return (
+    <AudioControlsStyled>
+      {isPlaying ? (
+        <AudioControlButton onClick={onPlayPauseClick}>
+          <Typography>정지</Typography>
+        </AudioControlButton>
+      ) : (
+        <AudioControlButton onClick={onPlayPauseClick}>
+          <Typography>재생</Typography>
+        </AudioControlButton>
+      )}
+      <AudioSoundControl
+        type="range"
+        value={volume}
+        step="1"
+        min="0"
+        max="100"
+        onChange={(e) => onAudioSoundControl(Number(e.target.value))}
+      />
+    </AudioControlsStyled>
+  );
+};
+
+const AudioControlsStyled = styled.div``;
+
+const AudioControlButton = styled.button``;
+
+const AudioSoundControl = styled.input``;
+
+export default AudioControls;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
-import Player from '~/components/Player';
-import Typography from './Typography';
+import AudioPlayer from '~/components/AudioPlayer';
 
 // TODO : 페이지 이동 시 렌더링 방지 필요 (노이즈 유지)
 const Header = () => {
@@ -16,22 +15,10 @@ const Header = () => {
           Noise
         </NoiseButton>
         <NoiseController show={show}>
-          <NoiseItem>
-            <Typography>장작소리</Typography>
-            <Player url="/audio/wood-fire.mp3" />
-          </NoiseItem>
-          <NoiseItem>
-            <Typography>파도소리</Typography>
-            <Player url="/audio/ocean.mp3" />
-          </NoiseItem>
-          <NoiseItem>
-            <Typography>빗소리</Typography>
-            <Player url="/audio/rain.mp3" />
-          </NoiseItem>
-          <NoiseItem>
-            <Typography>사람소리</Typography>
-            <Player url="/audio/people.mp3" />
-          </NoiseItem>
+          <AudioPlayer title="장작소리" url="/audio/wood-fire.mp3" />
+          <AudioPlayer title="파도소리" url="/audio/ocean.mp3" />
+          <AudioPlayer title="빗소리" url="/audio/rain.mp3" />
+          <AudioPlayer title="사람소리" url="/audio/people.mp3" />
         </NoiseController>
       </NoiseStyled>
     </HeaderStyled>
@@ -72,13 +59,6 @@ const NoiseController = styled.div<{ show: boolean }>`
   padding: 10px;
   border-radius: 30px;
   background-color: #f1f3f4;
-`;
-
-const NoiseItem = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
 `;
 
 export default Header;
