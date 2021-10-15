4554c4242ae95feae0bb19d2070332516845d413
leejiwonn <bbongwa@naver.com>
2021-10-15 18:35:26 +0900

    fix: `Home` 카테고리 및 노이즈 UI 수정

--- src/components/AudioControls.tsx
+++ src/components/AudioControls.tsx
@@ -27,14 +27,17 @@ const AudioControls = ({
           <Typography>재생</Typography>
         </AudioControlButton>
       )}
-      <AudioSoundControl
-        type="range"
-        value={volume}
-        step="1"
-        min="0"
-        max="100"
-        onChange={(e) => onAudioSoundControl(Number(e.target.value))}
-      />
+      <AudioSoundControlBox>
+        <AudioSoundControlBackground />
+        <AudioSoundControl
+          type="range"
+          value={volume}
+          step="1"
+          min="0"
+          max="100"
+          onChange={(e) => onAudioSoundControl(Number(e.target.value))}
+        />
+      </AudioSoundControlBox>
     </AudioControlsStyled>
   );
 };
@@ -48,13 +51,31 @@ const AudioControlButton = styled.button`
   padding: 0 10px;
 `;
 
+const AudioSoundControlBox = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const AudioSoundControlBackground = styled.div`
+  width: 100%;
+  height: 1px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  background-color: ${GraphicColor.BLACK};
+  z-index: 0;
+`;
+
 const AudioSoundControl = styled.input`
   -webkit-appearance: none;
   width: 120px;
-  height: 4px;
+  height: 7px;
   overflow: hidden;
   border-radius: 5px;
+  background-color: transparent;
   cursor: pointer;
+  z-index: 1;
 
   :focus {
     outline: none;
@@ -62,11 +83,11 @@ const AudioSoundControl = styled.input`
 
   ::-webkit-slider-thumb {
     -webkit-appearance: none;
-    width: 4px;
-    height: 4px;
-    background: ${GraphicColor.BLUE1_D};
+    width: 7px;
+    height: 7px;
+    background-color: ${GraphicColor.BLACK};
     border-radius: 50%;
-    box-shadow: -102px 0 0 100px ${GraphicColor.BLUE2_D};
+    box-shadow: 102px 0 0 100px ${GraphicColor.WHITE};
   }
 `;
 
