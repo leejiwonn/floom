af3aae7b0399acc39fcb44a3ed7bc07ad16efc7c
leejiwonn <bbongwa@naver.com>
2021-10-17 00:24:33 +0900

    fix: 음악 목록 업데이트

--- src/constants/music.ts
+++ src/constants/music.ts
@@ -1,4 +1,5 @@
 const MUSIC = {
+  // STUDY
   STUDY_01: {
     name: 'Nice piano and ukulele',
     author: 'Royalty',
@@ -22,6 +23,44 @@ const MUSIC = {
     author: 'Royalty',
     url: 'https://www.bensound.com/bensound-music/bensound-slowmotion.mp3',
     duration: '3:26',
+  },
+  // WORK
+  WORK_01: {
+    name: 'Nice piano and ukulele',
+    author: 'Royalty',
+    url: 'https://www.bensound.com/bensound-music/bensound-buddy.mp3',
+    duration: '2:02',
+  },
+  WORK_02: {
+    name: 'Gentle acoustic',
+    author: 'Acoustic',
+    url: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+    duration: '2:20',
+  },
+  WORK_03: {
+    name: 'Corporate motivational',
+    author: 'Corporate',
+    url: 'https://www.bensound.com/bensound-music/bensound-energy.mp3',
+    duration: '2:59',
+  },
+  // REST
+  REST_01: {
+    name: 'Nice piano and ukulele',
+    author: 'Royalty',
+    url: 'https://www.bensound.com/bensound-music/bensound-buddy.mp3',
+    duration: '2:02',
+  },
+  REST_02: {
+    name: 'Gentle acoustic',
+    author: 'Acoustic',
+    url: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3',
+    duration: '2:20',
+  },
+  REST_03: {
+    name: 'Corporate motivational',
+    author: 'Corporate',
+    url: 'https://www.bensound.com/bensound-music/bensound-energy.mp3',
+    duration: '2:59',
   },
 };
 
