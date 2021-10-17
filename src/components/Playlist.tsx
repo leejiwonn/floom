34ad15b264b30185fbfa115fc433114c47466ba4
Lee Jiwon <bbongwa123@gmail.com>
2021-10-17 15:17:16 +0900

    오디오 재생 중에 라우트 변경하면 유지안되던 문제 해결 (#3)
    * `.idea/` 무시
    
    * `visuallyHidden` 추가
    
    * 페이지 이동 시에도 오디오 재생 유지
    
    * `next/link` 사용
    
    * `next/link` 사용
    
    * fix

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from 'react';
 import MUSIC from '~/constants/music';
 
 import { BasicColor } from '~/utils/color';
+import { visuallyHidden } from '~/utils/css';
 import { FontType } from '~/utils/font';
 import PlaylistControls from './PlaylistControls';
 import Typography from './Typography';
@@ -14,74 +15,105 @@ interface Props {
   controls?: boolean;
 }
 
+// 저전력 모드 등에서는 play 재생 시에 오류 발생함
+const playAudioSafely = async (audio?: HTMLAudioElement) => {
+  if (audio == null) {
+    return;
+  }
+
+  try {
+    await audio.play();
+  } catch {
+    //
+  }
+};
+
+const formatTime = (currentTime: number) => {
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = Math.floor(currentTime % 60);
+  const currentsSeconds = seconds >= 10 ? seconds : '0' + (seconds % 60);
+
+  const formatTime = minutes + ':' + currentsSeconds;
+
+  return formatTime;
+};
+
 const Playlist = ({ playlist, controls }: Props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentTime, setCurrentTime] = useState('0:00');
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const playerRef = useRef(
-    typeof Audio !== 'undefined' && new Audio(MUSIC[playlist?.[0]]?.url),
-  );
+  const playerRef = useRef<HTMLAudioElement>(null);
 
   const timelineRef = useRef(null);
   const playheadRef = useRef(null);
 
   useEffect(() => {
     if (isPlaying) {
-      playerRef.current.play();
+      playAudioSafely(playerRef.current);
     } else {
-      playerRef.current.pause();
+      playerRef.current?.pause();
     }
   }, [isPlaying]);
 
   const updatePlayer = (index: number) => {
     const currentMusic = MUSIC[playlist[index]];
-    playerRef.current.src = currentMusic.url;
-    playerRef.current.load();
+
+    if (playerRef.current != null) {
+      playerRef.current.src = currentMusic.url;
+      playerRef.current.load();
+    }
   };
 
   const handlePrevButtonClick = () => {
     setCurrentIndex((prev) => (prev + playlist.length - 1) % playlist.length);
     updatePlayer(currentIndex);
     setIsPlaying(true);
-    playerRef.current.play();
+    playAudioSafely(playerRef.current);
   };
 
   const handleNextButtonClick = () => {
     setCurrentIndex((prev) => (prev + 1) % playlist.length);
     updatePlayer(currentIndex);
     setIsPlaying(true);
-    playerRef.current.play();
+    playAudioSafely(playerRef.current);
   };
 
   useEffect(() => {
-    playerRef.current.addEventListener('timeupdate', timeUpdate, false);
-  }, [playerRef]);
+    const audio = playerRef.current;
 
-  const timeUpdate = () => {
-    if (controls) {
-      const duration = playerRef.current.duration;
-      const playPercent = 100 * (playerRef.current.currentTime / duration);
-      playheadRef.current.style.width = playPercent + '%';
+    if (audio == null) {
+      return;
     }
-    const currentTime = formatTime(
-      parseInt(String(playerRef.current.currentTime)),
-    );
-    setCurrentTime(currentTime);
-  };
 
-  const formatTime = (currentTime: number) => {
-    const minutes = Math.floor(currentTime / 60);
-    const seconds = Math.floor(currentTime % 60);
-    const currentsSeconds = seconds >= 10 ? seconds : '0' + (seconds % 60);
+    const timeUpdate = () => {
+      if (audio == null) {
+        return;
+      }
 
-    const formatTime = minutes + ':' + currentsSeconds;
+      if (controls) {
+        const duration = audio.duration;
+        const playPercent = 100 * (audio.currentTime / duration);
+        audio.style.width = playPercent + '%';
+      }
+      const currentTime = formatTime(parseInt(String(audio.currentTime)));
+      setCurrentTime(currentTime);
+    };
 
-    return formatTime;
-  };
+    audio.addEventListener('timeupdate', timeUpdate, false);
+
+    return () => {
+      audio.removeEventListener('timeupdate', timeUpdate, false);
+    };
+  }, []);
 
   return (
     <PlaylistStyled>
+      <audio
+        ref={playerRef}
+        src={MUSIC[playlist?.[0]]?.url}
+        css={visuallyHidden}
+      />
       {controls && (
         <PlaylistControls
           music={MUSIC[playlist[currentIndex]]}
