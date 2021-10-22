8dc5eb8e92cabc84064df7ddc2feb478a3705fdb
leejiwonn <bbongwa@naver.com>
2021-10-22 10:29:05 +0900

    fix: 드롭다운 인지 가능하게 변경

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -94,7 +94,7 @@ const DropdownIconStyled = styled.span<{ active: boolean }>`
 
 const DropdownBox = styled.div<{ active: boolean }>`
   width: 100%;
-  height: 100px;
+  height: 15vh;
   display: ${({ active }) => (active ? 'flex' : 'none')};
   flex-direction: column;
   overflow-x: hidden;
