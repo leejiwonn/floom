6203e4c4c40355f0178f158b858d2c78eea7261f
leejiwonn <bbongwa@naver.com>
2021-10-24 18:15:45 +0900

    fix: 노이즈 슬라이더 UI 수정

--- src/components/AudioControls.tsx
+++ src/components/AudioControls.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+
 import { BasicColor } from '~/utils/color';
 
 import PlayIcon from '../../public/assets/icons/icon-play.svg';
@@ -28,17 +29,15 @@ const AudioControls = ({
           <PlayIcon fill={BasicColor.BLACK} />
         </AudioControlButton>
       )}
-      <AudioSoundControlBox>
-        <AudioSoundControlBackground />
-        <AudioSoundControl
-          type="range"
-          value={volume}
-          step="1"
-          min="0"
-          max="100"
-          onChange={(e) => onAudioSoundControl(Number(e.target.value))}
-        />
-      </AudioSoundControlBox>
+      <AudioSoundControl
+        type="range"
+        value={volume}
+        step="1"
+        min="0"
+        max="100"
+        onChange={(e) => onAudioSoundControl(Number(e.target.value))}
+        volume={volume}
+      />
     </AudioControlsStyled>
   );
 };
@@ -53,29 +52,17 @@ const AudioControlButton = styled.button`
   justify-content: center;
   align-items: center;
   padding-right: 10px;
-`;
 
-const AudioSoundControlBox = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
+  svg {
+    pointer-events: none;
   }
 `;
 
-const AudioSoundControlBackground = styled.div`
-  width: 100%;
-  height: 1px;
-  position: absolute;
-  left: 0;
-  right: 0;
-  background-color: ${BasicColor.BLACK};
-  z-index: 0;
-`;
-
-const AudioSoundControl = styled.input`
+const AudioSoundControl = styled.input<{ volume: number }>`
   -webkit-appearance: none;
   width: 80px;
   height: 7px;
+  position: relative;
   overflow: hidden;
   border-radius: 5px;
   background-color: transparent;
@@ -86,13 +73,44 @@ const AudioSoundControl = styled.input`
     outline: none;
   }
 
+  ::before {
+    position: absolute;
+    content: '';
+    top: 3px;
+    left: 0;
+    width: ${({ volume }) => volume + '%'};
+    height: 1px;
+    background-color: ${BasicColor.BLACK};
+    cursor: pointer;
+  }
+
   ::-webkit-slider-thumb {
     -webkit-appearance: none;
     width: 7px;
     height: 7px;
+    position: relative;
+    top: -3px;
     background-color: ${BasicColor.BLACK};
     border-radius: 50%;
-    box-shadow: 102px 0 0 100px ${BasicColor.WHITE};
+  }
+
+  ::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 1px;
+    cursor: pointer;
+    background: ${BasicColor.GRAY60};
+  }
+  ::-moz-range-track {
+    width: 100%;
+    height: 1px;
+    cursor: pointer;
+    background: ${BasicColor.GRAY60};
+  }
+  ::-ms-fill-upper {
+    width: 100%;
+    height: 1px;
+    cursor: pointer;
+    background: ${BasicColor.GRAY60};
   }
 `;
 
