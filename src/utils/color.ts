e7df25d3bdf6378ae1ba3a64a6f5d46c0cbc18a6
leejiwonn <bbongwa@naver.com>
2021-10-25 00:37:27 +0900

    fix: 방 색상, 밝기 변경 기능 추가

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -24,7 +24,26 @@ const BasicColor = {
   GREEN20: '#D1ECE0',
   GREEN10: '#E8F5EF',
 
-  YELLOW: '#FFCA42',
+  RED100: '#F47053',
+  RED70: '#F9AA98',
+  RED30: '#FAEBE8',
+
+  YELLOW100: '#FFEC44',
+  YELLOW70: '#FFF6A1',
+  YELLOW30: '#FFFBDA',
+
+  LIGHT_GREEN100: '#7AD3A8',
+  LIGHT_GREEN70: '#BCE9D3',
+  LIGHT_GREEN30: '#E4F6EE',
+
+  LIGHT_BLUE100: '#85B6FF',
+  LIGHT_BLUE70: '#C2DAFF',
+  LIGHT_BLUE30: '#E7F0FF',
+
+  PURPLE100: '#B598F2',
+  PURPLE70: '#DACCF9',
+  PURPLE30: '#F0EAFC',
+
   WHITE: '#FFFFFF',
   BLACK: '#000000',
 } as const;
@@ -34,7 +53,32 @@ const GradientColor = {
   GREEN: 'linear-gradient(to bottom right, #5CE8A4 0%, #3BD88D 100%)',
 } as const;
 
+const WallColor = {
+  RED_ONE: BasicColor.RED100,
+  RED_TWO: BasicColor.RED70,
+  RED_THREE: BasicColor.RED30,
+  YELLOW_ONE: BasicColor.YELLOW100,
+  YELLOW_TWO: BasicColor.YELLOW70,
+  YELLOW_THREE: BasicColor.YELLOW30,
+  GREEN_ONE: BasicColor.LIGHT_GREEN100,
+  GREEN_TWO: BasicColor.LIGHT_GREEN70,
+  GREEN_THREE: BasicColor.LIGHT_GREEN30,
+  BLUE_ONE: BasicColor.LIGHT_BLUE100,
+  BLUE_TWO: BasicColor.LIGHT_BLUE70,
+  BLUE_THREE: BasicColor.LIGHT_BLUE30,
+  PURPLE_ONE: BasicColor.PURPLE100,
+  PURPLE_TWO: BasicColor.PURPLE70,
+  PURPLE_THREE: BasicColor.PURPLE30,
+} as const;
+
 type BasicColor = typeof BasicColor[keyof typeof BasicColor];
 type GradientColor = typeof GradientColor[keyof typeof GradientColor];
+type WallColor = typeof WallColor[keyof typeof WallColor];
 
-export { BasicColor, GradientColor };
+export const getWallColor = (wallColor: string, light?: string) => {
+  return light
+    ? WallColor[(wallColor + `_${light}`) as keyof typeof WallColor]
+    : WallColor[(wallColor + '_ONE') as keyof typeof WallColor];
+};
+
+export { BasicColor, GradientColor, WallColor };
