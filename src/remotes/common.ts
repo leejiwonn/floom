316e66e35cffe17721011c495e149de42166de82
leejiwonn <bbongwa@naver.com>
2021-10-28 20:01:56 +0900

    fix: 방 이미지 캡쳐 후 저장 기능 구현

--- src/remotes/common.ts
+++ src/remotes/common.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { nanoid } from 'nanoid';
+
 import { PreUploadResponseData } from '~/types/common';
 import api from '~/utils/api';
 
@@ -7,6 +9,35 @@ export async function upload(file: File) {
     data: { url, key, requestUrl, requestFields },
   } = await api.post<PreUploadResponseData>('/api/common/pre-upload', {
     filename: file.name,
+    contentType: file.type,
+  });
+
+  const formData = new FormData();
+  formData.append('Content-Type', file.type);
+  Object.entries(requestFields).forEach(([k, v]) => {
+    formData.append(k, v);
+  });
+  formData.append('file', file);
+
+  await axios.post<void>(requestUrl, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return {
+    key,
+    url,
+  };
+}
+
+export async function uploadRoomImage(file: Blob) {
+  const num = nanoid();
+
+  const {
+    data: { url, key, requestUrl, requestFields },
+  } = await api.post<PreUploadResponseData>('/api/common/pre-upload', {
+    filename: `roomImage-${num}`,
     contentType: file.type,
   });
 
