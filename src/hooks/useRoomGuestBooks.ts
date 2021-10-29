445fb005c456c95c78599c97fb2a64d4fe769d70
leejiwonn <bbongwa@naver.com>
2021-10-30 00:22:08 +0900

    fix: 방명록 api 연동 & 추가 ui 구현

--- src/hooks/useRoomGuestBooks.ts
+++ src/hooks/useRoomGuestBooks.ts
@@ -1,15 +1,15 @@
+import { useMemo } from 'react';
 import { KeyLoader } from 'swr';
-import useSWRInfinite from 'swr/infinite';
-import { SWRInfiniteConfiguration } from 'swr/infinite/dist/infinite/types';
+import useSWRInfinite, { SWRInfiniteConfiguration } from 'swr/infinite';
 import { fetchRoomGuestBooks, FetchRoomGuestBooksParams } from '~/remotes/room';
-import { Paginated, PaginationOptions } from '~/types/pagination';
+import { Paginated } from '~/types/pagination';
 import { RoomGuestBook } from '~/types/RoomGuestBook';
 
 type Data = Paginated<RoomGuestBook>;
 
 const createKey =
   (roomId: number, limit?: number): KeyLoader<Data> =>
-  (page, prev) => {
+  (pageIndex, prev) => {
     // 마지막 페이지
     if (prev != null && prev.meta.currentPage === prev.meta.totalPages) {
       return null;
@@ -17,23 +17,26 @@ const createKey =
 
     const params: FetchRoomGuestBooksParams = {
       roomId,
-      page,
+      page: pageIndex + 1,
       limit,
     };
 
-    return [params];
+    return ['fetchRoomGuestBooks', JSON.stringify(params)];
   };
 
-type Options = Partial<Pick<PaginationOptions, 'limit'>> &
-  SWRInfiniteConfiguration<Data>;
+const fetcher = (_: unknown, params: string) => {
+  return fetchRoomGuestBooks(JSON.parse(params));
+};
+
+type Options = SWRInfiniteConfiguration<Data> & {
+  limit?: number;
+};
 
 export function useRoomGuestBooks(
   roomId: number,
   { limit, ...options }: Options = {},
 ) {
-  return useSWRInfinite(
-    createKey(roomId, limit),
-    (params: FetchRoomGuestBooksParams) => fetchRoomGuestBooks(params),
-    options,
-  );
+  const key = useMemo(() => createKey(roomId, limit), [roomId, limit]);
+
+  return useSWRInfinite(key, fetcher, options);
 }
