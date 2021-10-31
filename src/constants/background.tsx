b899a241e0596cd7f5a5adece4472ca90e9aec55
leejiwonn <bbongwa@naver.com>
2021-11-01 00:48:06 +0900

    fix: 풍경 사진 변경

--- src/constants/background.tsx
+++ src/constants/background.tsx
@@ -3,11 +3,11 @@ export type Background = 'RAIN' | 'SNOW' | 'SUNNY' | 'BLUR' | 'NIGHT';
 type BackgroundAssets = Record<Background, string>;
 
 const BACKGROUND: BackgroundAssets = {
-  RAIN: '/assets/images/background/background-rain.jpeg',
-  SNOW: '/assets/images/background/background-snow.jpeg',
-  SUNNY: '/assets/images/background/background-sunny.jpeg',
-  BLUR: '/assets/images/background/background-blur.jpeg',
-  NIGHT: '/assets/images/background/background-night.jpeg',
+  RAIN: '/assets/images/background/rain.gif',
+  SNOW: '/assets/images/background/snow.gif',
+  SUNNY: '/assets/images/background/sunny.gif',
+  BLUR: '/assets/images/background/blur.gif',
+  NIGHT: '/assets/images/background/night.gif',
 };
 
 export default BACKGROUND;
