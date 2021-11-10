d727ce35ebf98c33dc5212cf0781ef8a5a59b24e
leejiwonn <bbongwa@naver.com>
2021-11-10 11:03:00 +0900

    fix: 상세 화면 한 줄 소개 표시

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -64,25 +64,48 @@ const Detail = ({ room: initialRoom }: Props) => {
       <RoomImage url={room.roomImage} />
       <RoomInfoStyled>
         <RoomTitleStyled>
-          <RoomTitleInfo>
-            <Typography font={FontType.SEMI_BOLD_HEAD_03}>
-              {room.category.name}
-            </Typography>
-            <RoomTitleInfoLine />
-            <Typography font={FontType.EXTRA_BOLD_HEAD_03}>
-              {room.title}
-            </Typography>
-          </RoomTitleInfo>
-          <Typography
-            font={FontType.REGULAR_BODY}
-            color={BasicColor.DARK70}
-            marginBottom={2.4}
-          >
-            {room.creator.displayName} 님의 방
-          </Typography>
-          <TagStyled>
-            {room.tags.length > 0 ? (
-              room.tags.map((tag, index) => (
+          <RoomTitleInfoStyled>
+            <RoomTitleInfo>
+              <Typography font={FontType.SEMI_BOLD_HEAD_03}>
+                {room.category.name}
+              </Typography>
+              <RoomTitleInfoLine />
+              <Typography font={FontType.EXTRA_BOLD_HEAD_03}>
+                {room.title}
+              </Typography>
+            </RoomTitleInfo>
+            <CaptionStyled>
+              <CaptionItem>
+                <EmojiStyled style={{ opacity: 0.7 }}>{EMOJI.EYES}</EmojiStyled>
+                <Typography
+                  font={FontType.REGULAR_CAPTION}
+                  color={BasicColor.DARK70}
+                >
+                  방문
+                </Typography>
+                <Typography font={FontType.SEMI_BOLD_CAPTION} marginLeft={0.5}>
+                  {room.reviewsCount}
+                </Typography>
+              </CaptionItem>
+              <CaptionItem>
+                <EmojiStyled style={{ opacity: 0.7 }}>
+                  {EMOJI.RECOMMEND}
+                </EmojiStyled>
+                <Typography
+                  font={FontType.REGULAR_CAPTION}
+                  color={BasicColor.DARK70}
+                >
+                  추천
+                </Typography>
+                <Typography font={FontType.SEMI_BOLD_CAPTION} marginLeft={0.5}>
+                  {room.recommendReviewsCount}
+                </Typography>
+              </CaptionItem>
+            </CaptionStyled>
+          </RoomTitleInfoStyled>
+          {room.tags.length > 0 ? (
+            <TagStyled>
+              {room.tags.map((tag, index) => (
                 <TagItem key={tag + index}>
                   <Typography
                     font={FontType.SEMI_BOLD_BODY}
@@ -91,47 +114,19 @@ const Detail = ({ room: initialRoom }: Props) => {
                     {tag}
                   </Typography>
                 </TagItem>
-              ))
-            ) : (
-              <TagItem disable>
-                <Typography
-                  font={FontType.SEMI_BOLD_BODY}
-                  color={BasicColor.DARK70}
-                >
-                  태그를 등록하지 않았어요.
-                </Typography>
-              </TagItem>
-            )}
-          </TagStyled>
-          <CaptionStyled>
-            <CaptionItem>
-              <Typography
-                font={FontType.REGULAR_CAPTION}
-                color={BasicColor.DARK70}
-              >
-                방문
-              </Typography>
-              <Typography font={FontType.SEMI_BOLD_BODY} marginLeft={0.8}>
-                {room.reviewsCount}
-              </Typography>
-            </CaptionItem>
-            <CaptionLine />
-            <CaptionItem>
-              <Typography
-                font={FontType.REGULAR_CAPTION}
-                color={BasicColor.DARK70}
-              >
-                추천
-              </Typography>
-              <Typography font={FontType.SEMI_BOLD_BODY} marginLeft={0.8}>
-                {room.recommendReviewsCount}
-              </Typography>
-            </CaptionItem>
-          </CaptionStyled>
+              ))}
+            </TagStyled>
+          ) : null}
+          <Typography font={FontType.REGULAR_BODY} marginBottom={1}>
+            {room.description}
+          </Typography>
+          <Typography font={FontType.REGULAR_BODY} color={BasicColor.DARK70}>
+            {room.creator.displayName} 님의 방
+          </Typography>
         </RoomTitleStyled>
         <RoomContentStyled>
           <UserListStyled>
-            <Typography font={FontType.BOLD_TITLE_01} marginBottom={3}>
+            <Typography font={FontType.BOLD_TITLE_02} marginBottom={3}>
               다른 사람들은 이런 일에 몰입했어요!
             </Typography>
             {reviews != null ? (
@@ -182,10 +177,10 @@ const Detail = ({ room: initialRoom }: Props) => {
             ) : null}
           </UserListStyled>
           <PlaylistStyled>
-            <Typography font={FontType.BOLD_TITLE_01} marginBottom={1.6}>
+            <Typography font={FontType.BOLD_TITLE_02} marginBottom={1.6}>
               플레이리스트
             </Typography>
-            <Playlist playlist={room.musics} controls={false} viewHeight={44} />
+            <Playlist playlist={room.musics} controls={false} viewHeight={41} />
           </PlaylistStyled>
         </RoomContentStyled>
       </RoomInfoStyled>
@@ -237,18 +232,20 @@ const DetailStyled = styled.div`
 
 const RoomImage = styled.img<{ url: string }>`
   width: 100%;
-  height: 42vh;
+  height: 36vh;
   background-image: ${({ url }) => `url(${url})`};
   background-size: cover;
   background-position: 50%;
   background-repeat: no-repeat;
 `;
 
-const TagStyled = styled.div``;
+const TagStyled = styled.div`
+  margin-bottom: 3em;
+`;
 
 const TagItem = styled.div<{ disable?: boolean }>`
   display: inline-flex;
-  padding: 0.4em 1.2em;
+  padding: 0.4em 1em;
   background-color: ${({ disable }) =>
     disable ? BasicColor.DARK10 : BasicColor.GREEN10};
   border: 0.1em solid
@@ -267,17 +264,26 @@ const RoomInfoStyled = styled.div`
 `;
 
 const RoomTitleStyled = styled.div`
+  width: 45%;
   display: flex;
   flex-direction: column;
   margin-top: 4em;
   padding-left: 5em;
+  box-sizing: content-box;
+`;
+
+const RoomTitleInfoStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 2em;
 `;
 
 const RoomTitleInfo = styled.div`
   display: inline-flex;
   flex-direction: row;
   align-items: center;
-  margin-bottom: 1em;
 `;
 
 const RoomTitleInfoLine = styled.div`
@@ -290,42 +296,43 @@ const RoomTitleInfoLine = styled.div`
 const CaptionStyled = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: flex-end;
   align-items: center;
-  margin-top: 3.6em;
 `;
 
 const CaptionItem = styled.div`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-`;
-
-const CaptionLine = styled.div`
-  width: 0.1em;
-  height: 60%;
-  background-color: ${BasicColor.GRAY60};
-  margin: 0 1.5em;
+  background-color: ${BasicColor.DARK10};
+  border-radius: 0.9em;
+  padding: 1em;
+  margin-right: 2em;
 `;
 
 const RoomContentStyled = styled.div`
+  width: 55%;
   display: flex;
   flex-direction: row;
   padding-top: 4em;
+  box-sizing: content-box;
 `;
 
 const UserListStyled = styled.div`
+  width: calc(100% - 32em);
   border-left: 1px solid ${BasicColor.GRAY60};
-  padding: 0 3em;
+  padding: 0 2em;
+  box-sizing: content-box;
 `;
 
 const CommentStyled = styled.div`
-  width: auto;
-  height: 49vh;
+  width: 100%;
+  height: 46vh;
   overflow: auto;
 `;
 
 const CommentItem = styled.div`
-  width: 34em;
+  width: 100%;
   background-color: ${BasicColor.GRAY10};
   border-radius: 0 2em 2em 2em;
   padding: 1.5em 2em;
@@ -351,9 +358,11 @@ const EmojiStyled = styled.div`
 `;
 
 const PlaylistStyled = styled.div`
-  margin-right: 6em;
+  width: 32em;
+  margin-right: 5em;
   border-left: 0.1em solid ${BasicColor.GRAY60};
-  padding-left: 3em;
+  padding-left: 2em;
+  box-sizing: content-box;
 `;
 
 const PlayButton = styled.a`
