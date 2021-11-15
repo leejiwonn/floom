89ae68af582069fba07c853932920fde52416b12
leejiwonn <bbongwa@naver.com>
2021-11-15 21:06:12 +0900

    fix: 인트로 부분 헤더 숨김

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import Header from '~/components/Header';
 
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
-      <Header />
+      {router.pathname !== '/intro' && <Header />}
       <Component {...pageProps} />
     </>
   );
