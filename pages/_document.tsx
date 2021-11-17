1a53316fb579d30ca8161556cb3a23abf6b41066
leejiwonn <bbongwa@naver.com>
2021-11-17 23:45:08 +0900

    fix: 메타 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,13 @@ class MyDocument extends Document {
             type="text/css"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
+          <meta property="og:image" content="/static/og-image.png" />
+          <meta property="og:title" content="FLOOM" />
+          <meta
+            property="og:description"
+            content="쉽고 재미있게 공간을 탐방하며 몰입의 즐거움을 경험해보세요!"
+          />
+          <title>몰입의 즐거움을 경험하다, FLOOM!</title>
         </Head>
         <body>
           <Main />
